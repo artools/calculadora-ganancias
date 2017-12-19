91d1c289d662e96831bed5ccb2c1e3d60389ca33
Juan M. Romera <juanmanuel.romeraferrio@gmail.com>
2017-12-19 20:41:37 -0300

    version del 2017

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# calculadora-ganancias
-Calculadora de Ganancias
+# Ganancias
+[Calculadora de Ganancias Argentina](https://artools.github.io/ganancias2017/pages/index.html)
+
+## Log
+Ultima Actualizacion -> 19/12/2017 con estos [valores](http://afip.gob.ar/noticias/20171219ImpGanancias.asp) 
---
+++ js/calc.js
@@ -0,0 +1,74 @@
+function calcular() {
+    var sueldoBruto = $("#sueldoBruto").val(),
+        isConyuge = $("input[name='conyuge']:checked").val(),
+        isJubilado = $("input[name='jubilado']:checked").val(),
+        isPatagonico = $("input[name='patagonico']:checked").val(),
+        valorAlquiler = $("#alquiler").val(),
+        deduccionAlquiler = 12 * valorAlquiler * .4 > TOPE_ALQUILER ? TOPE_ALQUILER : 12 * valorAlquiler * .4,
+        valorHipotecario = $("#hipotecario").val(),
+        deduccionHipotecario = 12 * valorHipotecario > TOPE_HIPOTECARIO ? TOPE_HIPOTECARIO : 12 * valorHipotecario,
+		hijosElement = document.getElementById("hijos"),
+        cantHijos = hijosElement.options[hijosElement.selectedIndex].value,
+        sueldoNeto = 0 == isJubilado ? .17 * sueldoBruto > TOPE_APORTES ? sueldoBruto - TOPE_APORTES : .83 * sueldoBruto : .06 * sueldoBruto > TOPE_APORTES ? sueldoBruto - TOPE_APORTES : .94 * sueldoBruto;
+
+    var sueldoNetoAnual = 13 * sueldoNeto,
+        mniConDeduccionEspecial = (MINIMO_NO_IMPONIBLE + ADICIONAL_4TA_CATEGORIA) * (1.22 * isPatagonico + (1 - isPatagonico));
+    mniTotal = (mniConDeduccionEspecial + CONYUGE * isConyuge + HIJO * cantHijos + deduccionAlquiler + deduccionHipotecario) * (1 - isJubilado) + isJubilado * (TOPE_JUBILADO + deduccionAlquiler + deduccionHipotecario),
+        montoImponibleAplicable = 0,
+        mniTotal < sueldoNetoAnual && (montoImponibleAplicable = sueldoNetoAnual - mniTotal);
+    
+    var result = calcularImpuesto(montoImponibleAplicable);
+    impuestoAnual = result.value.toFixed(2), $("#impuestoAnual").text("$" + impuestoAnual);
+
+    var impuestoMensual = (impuestoAnual / 13).toFixed(2);
+    $("#impuestoMensual").text("$" + impuestoMensual);
+    var alicuota = impuestoMensual / sueldoBruto * 100;
+    $("#alicuota").text(alicuota.toFixed(2) + "%");
+    var alicuotaMarginal = 0 == alicuota ? 0 : 100 * porcentajesEscalas[result.escala];
+    $("#alicuotaMarginal").text(alicuotaMarginal.toFixed(2) + "%");
+    var sueldoEnMano = sueldoNeto - impuestoMensual;
+    $("#sueldoEnMano").text("$" + Math.round(sueldoEnMano) + ".00")
+}
+
+function calcularImpuesto(monto) {
+    var i = 0;
+    var result = {};
+    var value = 0;
+    while(monto > topesEscalas[i]) {
+        var diff = i == 0 ? topesEscalas[i] : topesEscalas[i] - topesEscalas[i - 1];
+        value += diff * porcentajesEscalas[i];
+        i++;
+    }
+
+    diff = i == 0 ? monto : monto - topesEscalas[i - 1];
+    value += diff * porcentajesEscalas[i];
+
+    result.value = value;
+    result.escala = i;
+    return result;
+}
+
+$(document).ready(function() {
+    $("#calcular").on("click", function() {
+        calcular()
+    }), $(document).keypress(function(a) {
+        13 == a.keyCode && (a.preventDefault(), calcular())
+    }), $("input[name='jubilado']").click(function() {
+        $("input[name='conyuge']").attr({
+            disabled: 1 == $(this).val()
+        }), $("input[name='patagonico']").attr({
+            disabled: 1 == $(this).val()
+        })
+    })
+});
+var topesEscalas = [2e4, 4e4, 6e4, 8e4, 12e4, 16e4, 24e4, 32e4, 99999999],
+    porcentajesEscalas = [.05, .09, .12, .15, .19, .23, .27, .31, .35],
+    fijosEscalas = [1e3, 1800, 2400, 3e3, 7600, 9200, 21600, 24800],
+    MINIMO_NO_IMPONIBLE = 51967,
+    ADICIONAL_4TA_CATEGORIA = 249441.6,
+    CONYUGE = 48447,
+    HIJO = 24432,
+    TOPE_APORTES = 10896.32,
+    TOPE_JUBILADO = 407592,
+    TOPE_ALQUILER = 51967,
+    TOPE_HIPOTECARIO = 20000;
